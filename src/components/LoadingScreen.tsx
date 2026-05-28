f12a70f73a1569251afa30bdcdd328da63cc153d
ibrahim mohamed <ibrahimmohamed@192.168.1.11>
2026-05-28 05:14:11 +0300

    update: loading screen with logo image

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -27,65 +27,14 @@ export default function LoadingScreen() {
             justifyContent: "center",
           }}
         >
-          {/* IM */}
-          <motion.div
+          <motion.img
+            src="/loading.png"
+            alt="IM Design Studio"
             initial={{ opacity: 0, y: 24 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.2 }}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "2px",
-              marginBottom: "12px",
-            }}
-          >
-            <span style={{
-              fontSize: "3.5rem",
-              fontWeight: "800",
-              color: "#213A70",
-              lineHeight: 1,
-              letterSpacing: "-2px",
-            }}>I</span>
-            <span style={{
-              fontSize: "3.5rem",
-              fontWeight: "800",
-              color: "#213A70",
-              lineHeight: 1,
-              letterSpacing: "-2px",
-            }}>M</span>
-          </motion.div>
-
-          {/* Design Studio */}
-          <motion.p
-            initial={{ opacity: 0, y: 16 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, delay: 0.5 }}
-            style={{
-              color: "#D68A4E",
-              fontSize: "1.6rem",
-              fontWeight: "600",
-              letterSpacing: "0.05em",
-              margin: "0 0 10px 0",
-            }}
-          >
-            Design Studio
-          </motion.p>
-
-          {/* Tagline */}
-          <motion.p
-            initial={{ opacity: 0, y: 16 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, delay: 0.8 }}
-            style={{
-              color: "#D68A4E99",
-              fontSize: "0.65rem",
-              letterSpacing: "0.3em",
-              textTransform: "uppercase",
-              margin: 0,
-            }}
-          >
-            Where Ink Meets Mind
-          </motion.p>
+            style={{ width: "220px", objectFit: "contain" }}
+          />
         </motion.div>
       )}
     </AnimatePresence>
